3617878a5782d8c7451f4914982ab4696b9f4480
robalmeister <filipw2701@gmail.com>
2024-02-21 13:28:34 +0100

    export const runtime = 'edge';

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     <header className="sticky flex justify-center border-b">
       <div className="flex items-center justify-between w-full h-16 max-w-3xl px-4 mx-auto sm:px-6">
         <MainNav />
-        <UserButton />
+
       </div>
     </header>
   )
